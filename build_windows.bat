db7c1b9de127e0136ca71fb82505a5a3fa8749bf
ChuHuyNam <93912455+ChuHuyNam@users.noreply.github.com>
2026-08-23 23:30:14 +0700

    revert: remove build_windows.bat

--- build_windows.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-py -m pip install --upgrade pyinstaller
-if errorlevel 1 exit /b 1
-py -m PyInstaller --noconfirm --clean --onefile --windowed --name HornerCalculator horner_calculator.py
-if errorlevel 1 exit /b 1
-echo.
-echo Da tao: dist\HornerCalculator.exe
-endlocal
